b63ba8920a339e586ad358d7fa15a59f028b5ba2
LenaSevast <blockada2013@yandex.ru>
2022-10-03 12:55:22 +0300

    fixing

--- main.py
+++ main.py
@@ -2,14 +2,11 @@
 a = int(input("Введите  первое число "))
 b = int(input("Введите второе число "))
 s = a + b
-print("Сумма ")
-print(s)
+print("Сумма ", s)
 ss = a - b
 print("Разность ", ss)
 sss = a * b
-print("Производное ")
-print(sss)
+print("Производное ", sss)
 ssss = a / b
-print("Частное ")
-print(ssss)
+print("Частное ", ssss)
 
