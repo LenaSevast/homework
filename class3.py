fd39b25a56f06d783c11363b5ed132610ff52d32
LenaSevast <114513581+LenaSevast@users.noreply.github.com>
2022-11-06 20:34:25 +0300

    Delete class3.py

--- class3.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Введите число "))
-for x in range(0, n + 1 ,2):
-    print(x)
-
-p = int(input("Введите число "))
-for i in range(1, p // 2 + 1, 1):
-    if p % i == 0:
-        print(i)
-
